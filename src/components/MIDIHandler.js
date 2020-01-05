7c4389c3651fb3779b101cf4f1ec9bd99556e199
Grégory Betton <contact@grebett.com>
2020-01-05 01:20:38 +0100

    basic midi handler

---
+++ src/components/MIDIHandler.js
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux';
+
+const MIDIHandler = () => {
+  const dispatch = useDispatch();
+  const onMIDIMessage = ({ data, ...rest }) => {
+    const [command, value, velocity] = data;
+    // const pos = (1 / 127) * velocity
+    // const MIDIValue = mojsCurve.getMIDIValue()(pos);
+    // mojsCurve.updateProgressLine(pos);
+    dispatch({ type: 'INCOMING_MIDI_DATA', payload: { command, value, velocity } });
+  };
+
+  (async () => {
+    if (navigator.requestMIDIAccess) {
+      const midi = await navigator.requestMIDIAccess();
+      const inputs = midi.inputs.values();
+      // loop over all available inputs and listen for any MIDI input
+      for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+        if (input.value.name.indexOf('Touchbar') !== -1) {
+          input.value.onmidimessage = onMIDIMessage;
+        }
+      }
+      console.log(midi);
+    } else {
+      throw new Error('MIDI is not supported on your device!');
+    }
+  })();
+
+  return null;
+};
+
+export { MIDIHandler };

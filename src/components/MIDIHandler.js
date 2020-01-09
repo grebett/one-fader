7cff9e17500b877a5b0d98791c787a3b699162ee
Grégory Betton <contact@grebett.com>
2020-01-09 12:05:11 +0100

    cleaner

--- src/components/MIDIHandler.js
+++ src/components/MIDIHandler.js
@@ -67,29 +67,31 @@ const MIDIHandler = () => {
     };
     proxyAllMIDIMessages(divisimatePorts, IACDriverBuses);
 
-    // // One Fader to rule them all, One Fader to find them,
-    // // One Fader to bring them all and in the music bind them
-    // // In the Land of MIDI where the CCs lie.
-    // store.subscribe(() => { // TODO: we should make the first call before a subscribe !
-    //   MIDITouchbar.removeListener('controlchange', 'all');
-    //   const editors = store.getState().app.curveEditors;
-    //   console.log(editors);
-    //   MIDITouchbar.addListener('controlchange', 'all', ({ data }) => {
-    //     const [, inputCC, inputValue] = data;
-    //     if (inputCC === 1) {
-    //       for (let i = 0; i < editors.length; i++) {
-    //         const editor = editors[i];
-    //         const outputValue = editor.MIDIValues[inputValue];
-    //         const output = outputs[parseInt(editor.instrument, 10) - 1];
-    //         const outputCC = parseInt(editor.CC, 10);
-    //         console.log(outputCC, outputValue);
-    //         output.sendControlChange(outputCC, outputValue);
-    //       }
-    //     }
-    //   });
-    // });
+    // One Fader to rule them all, One Fader to find them,
+    // One Fader to bring them all and in the music bind them
+    // In the Land of MIDI where the CCs lie.
+    const oneFader = () => {
+      controllers[0].removeListener('controlchange', 'all');
+      const editors = store.getState().app.curveEditors;
+      // all channels ???
+      controllers[0].addListener('controlchange', 'all', ({ data }) => {
+        console.log(data);
+        const [, inputCC, inputValue] = data;
+        if (inputCC === 1) {
+          for (let i = 0; i < editors.length; i++) {
+            const editor = editors[i];
+            const outputValue = editor.MIDIValues[inputValue];
+            const output = IACDriverBuses[parseInt(editor.instrument, 10) - 1];
+            const outputCC = parseInt(editor.CC, 10);
+            console.log(outputCC, outputValue);
+            output.sendControlChange(outputCC, outputValue);
+          }
+        }
+      });
+    };
+    oneFader();
+    store.subscribe(oneFader);
   });
-
   return reduxStore => (store = reduxStore);
 };
 
